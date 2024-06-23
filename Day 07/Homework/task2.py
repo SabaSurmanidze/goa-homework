5581ce13272a7cce5922a63f58cff8f3c6a94359
saba surmanidze <surmanidzemerab2@gmail.com>
2024-06-23 13:26:53 +0400

    commit

--- Day 07/Homework/task2.py
+++ Day 07/Homework/task2.py
@@ -1,7 +1,9 @@
+#2) დაწერეთ პროგრამა სადაც შემოიტანთ თქვენი ოჯახის წევრების ასაკს და გამოიტანეთ ის თუ რამდენი წლის იქნებიან 20 წლის შემდეგ.
+
 mums_age = 33
 dad_age = 44
 brother_age = 5
-yung_brother_age = 1
+young_brother_age = 1
 grandma_age = 62
 grandpa_age = 63
 my_age = 13
@@ -9,7 +11,7 @@ my_age = 13
 print(mums_age + 20)
 print(dad_age + 20)
 print(brother_age + 20)
-print(yung_brother_age + 20)
+print(young_brother_age + 20)
 print(grandma_age + 62)
 print(grandpa_age + 20)
 print(my_age + 20)
--- Day 07/Homework/task4.py
+++ Day 07/Homework/task4.py
@@ -1,4 +1,4 @@
+#4)დაწერეთ მარტივი პროგრამა, რომელიც სთხოვს მომხმარებელს შეიყვანოს თავისი სახელი და შემდეგ დაბეჭდოს მისალოცი შეტყობინება მათი სახელის გამოყენებით.
 name = input('pls enter your name:')
 greeting_card = input('write a  greeting card using your name:')
-print(name)
-print(greeting_card)
+print(name + greeting_card)

3ca5a74a5b818a9aba10b87414a731fa6fc025f3
saba surmanidze <surmanidzemerab2@gmail.com>
2024-08-02 15:56:12 +0400

    commit

---
+++ Day 13/Homework/task2.py
@@ -0,0 +1,77 @@
+#2) ივარჯიშეთ განვლილ მასალაზე, ყველა ნასწავლ საკითხზე გააკეთეთ 10 მაგალითი.
+
+
+
+print(10+10)
+print(25-15)
+print(15*15)
+print(200/2)
+print(10**10)
+print(1000//10)
+print(235+245)
+print(100-67)
+print(15*100)
+print(50//2)
+
+
+
+
+
+name = "saba"
+surname = "surmanidze"
+age = 13
+print(name,surname,age)
+
+
+
+
+number = int(input("enter number"))
+Number = int(input("enter Number"))
+print(number*Number)
+
+
+
+print(5<10)
+print(6>3)
+print(8==8)
+print(8>=6)
+print(20>10)
+print(9==9)
+print(100!=200)
+print(9<=9)
+print(10>5)
+print(100!=56)
+print(8<67)
+
+
+
+
+
+
+age = int(input("enter your age:"))
+my_age = 13
+print(age==my_age)
+
+
+
+
+number = int(input("enter number:"))
+print(number>0)
+print(10>number)
+
+
+
+my_age = 13
+print(my_age+20)
+
+
+
+name = "saba"
+last_name = "surmanidze"
+age = 13
+
+print('i am ',name ,surname,'and i am ',age, 'years old',)
+
+
+
+
